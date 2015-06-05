6a453a8a2ee9d7fe51f402f564fd661a3778cc4c
Syoyo Fujita <syoyo@lighttransport.com>
2015-06-05 16:03:28 +0900

    Fix compile with C compiler.

---
+++ test/c-binding/test.c
@@ -0,0 +1,17 @@
+#include <tinyexr.h>
+
+int main(int argc, char** argv)
+{
+  float *rgba;
+  const char* err;
+  int width;
+  int height;
+
+  if (argc < 2) {
+    return EXIT_FAILURE;
+  }
+
+  int ret = LoadEXR(&rgba, &width, &height, argv[1], &err);
+
+  return ret; 
+}
