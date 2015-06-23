d0993a9099f1542e9d43be2f29d961562988d4d9
Syoyo Fujita <syoyo@lighttransport.com>
2015-06-23 11:56:56 +0900

    Change to use expanding tab as whitespace...

--- tinyexr.h
+++ tinyexr.h
@@ -4,14 +4,14 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-		* Redistributions of source code must retain the above copyright
-			notice, this list of conditions and the following disclaimer.
-		* Redistributions in binary form must reproduce the above copyright
-			notice, this list of conditions and the following disclaimer in the
-			documentation and/or other materials provided with the distribution.
-		* Neither the name of the <organization> nor the
-			names of its contributors may be used to endorse or promote products
-			derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -39,29 +39,29 @@ extern "C" {
 #define TINYEXR_PIXELTYPE_FLOAT (2)
 
 typedef struct _EXRImage {
-	int num_channels;
-	const char **channel_names;
+  int num_channels;
+  const char **channel_names;
 
-	unsigned char **images; // image[channels][pixels]
-	int *pixel_types; // Loaded pixel type(TINYEXR_PIXELTYPE_*) of `images` for
-										// each channel
+  unsigned char **images; // image[channels][pixels]
+  int *pixel_types; // Loaded pixel type(TINYEXR_PIXELTYPE_*) of `images` for
+                    // each channel
 
-	int *requested_pixel_types; // Filled initially by
-															// ParseEXRHeaderFrom(Meomory|File), then users
-															// can edit it(only valid for HALF pixel type
-															// channel)
+  int *requested_pixel_types; // Filled initially by
+                              // ParseEXRHeaderFrom(Meomory|File), then users
+                              // can edit it(only valid for HALF pixel type
+                              // channel)
 
-	int width;
-	int height;
+  int width;
+  int height;
 } EXRImage;
 
 typedef struct _DeepImage {
-	int num_channels;
-	const char **channel_names;
-	float ***image;			// image[channels][scanlines][samples]
-	int **offset_table; // offset_table[scanline][offsets]
-	int width;
-	int height;
+  int num_channels;
+  const char **channel_names;
+  float ***image;     // image[channels][scanlines][samples]
+  int **offset_table; // offset_table[scanline][offsets]
+  int width;
+  int height;
 } DeepImage;
 
 // @deprecated { to be removed. }
@@ -71,23 +71,23 @@ typedef struct _DeepImage {
 // Return 0 if success
 // Returns error string in `err` when there's an error
 extern int LoadEXR(float **out_rgba, int *width, int *height,
-									 const char *filename, const char **err);
+                   const char *filename, const char **err);
 
 // Parse single-frame OpenEXR header from a file and initialize `EXRImage`
 // struct.
 // Users then call LoadMultiChannelEXRFromFile to actually load image data into
 // `EXRImage`
 extern int ParseMultiChannelEXRHeaderFromFile(EXRImage *image,
-																							const char *filename,
-																							const char **err);
+                                              const char *filename,
+                                              const char **err);
 
 // Parse single-frame OpenEXR header from a memory and initialize `EXRImage`
 // struct.
 // Users then call LoadMultiChannelEXRFromMemory to actually load image data
 // into `EXRImage`
 extern int ParseMultiChannelEXRHeaderFromMemory(EXRImage *image,
-																								const unsigned char *memory,
-																								const char **err);
+                                                const unsigned char *memory,
+                                                const char **err);
 
 // Loads multi-channel, single-frame OpenEXR image from a file.
 // Application must setup `ParseMultiChannelEXRHeaderFromFile` before calling
@@ -96,7 +96,7 @@ extern int ParseMultiChannelEXRHeaderFromMemory(EXRImage *image,
 // Return 0 if success
 // Returns error string in `err` when there's an error
 extern int LoadMultiChannelEXRFromFile(EXRImage *image, const char *filename,
-																			 const char **err);
+                                       const char **err);
 
 // Loads multi-channel, single-frame OpenEXR image from a memory.
 // Application must setup `EXRImage` with `ParseMultiChannelEXRHeaderFromMemory`
@@ -105,8 +105,8 @@ extern int LoadMultiChannelEXRFromFile(EXRImage *image, const char *filename,
 // Return 0 if success
 // Returns error string in `err` when there's an error
 extern int LoadMultiChannelEXRFromMemory(EXRImage *image,
-																				 const unsigned char *memory,
-																				 const char **err);
+                                         const unsigned char *memory,
+                                         const char **err);
 
 // Saves floating point RGBA image as OpenEXR.
 // Image is compressed with ZIP.
@@ -120,7 +120,7 @@ extern int LoadMultiChannelEXRFromMemory(EXRImage *image,
 // Returns 0 if success
 // Returns error string in `err` when there's an error
 extern int SaveMultiChannelEXRToFile(const EXRImage *image,
-																		 const char *filename, const char **err);
+                                     const char *filename, const char **err);
 
 // Saves multi-channel, single-frame OpenEXR image to a memory.
 // Application must free EXRImage
@@ -128,15 +128,15 @@ extern int SaveMultiChannelEXRToFile(const EXRImage *image,
 // Retruns 0 if success, negative number when failed.
 // Returns error string in `err` when there's an error
 extern size_t SaveMultiChannelEXRToMemory(const EXRImage *image,
-																					unsigned char **memory,
-																					const char **err);
+                                          unsigned char **memory,
+                                          const char **err);
 
 // Loads single-frame OpenEXR deep image.
 // Application must free memory of variables in DeepImage(image, offset_table)
 // Returns 0 if success
 // Returns error string in `err` when there's an error
 extern int LoadDeepEXR(DeepImage *out_image, const char *filename,
-											 const char **err);
+                       const char **err);
 
 // NOT YET IMPLEMENTED:
 // Saves single-frame OpenEXR deep image.
@@ -163,7 +163,7 @@ extern int FreeExrImage(EXRImage *exrImage);
 // Parse single-frame OpenEXR header from memory.
 // Return 0 if success
 extern int ParseEXRHeaderFromMemory(int *width, int *height,
-																		const unsigned char *memory);
+                                    const unsigned char *memory);
 
 // For emscripten.
 // Loads single-frame OpenEXR image from memory. Assume EXR image contains
@@ -173,8 +173,7 @@ extern int ParseEXRHeaderFromMemory(int *width, int *height,
 // Return 0 if success
 // Returns error string in `err` when there's an error
 extern int LoadEXRFromMemory(float *out_rgba, const unsigned char *memory,
-														 const char **err);
-
+                             const char **err);
 
 #ifdef __cplusplus
 }
