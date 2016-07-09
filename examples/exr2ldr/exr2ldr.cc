44716f66691ab9e3280eb393e72608c0538781fd
Syoyo Fujita <syoyo@lighttransport.com>
2016-07-09 16:56:37 +0900

    Add option to specify gamma value.

--- examples/exr2ldr/exr2ldr.cc
+++ examples/exr2ldr/exr2ldr.cc
@@ -10,22 +10,22 @@
 
 #include "tinyexr.h"
 
-inline unsigned char ftouc(float f)
+inline unsigned char ftouc(float f, float gamma)
 {
-  int i = static_cast<int>(255.0f * powf(f, 1.0f / 2.2f)); // simple gamma correction.
+  int i = static_cast<int>(255.0f * powf(f, 1.0f / gamma));
   if (i > 255) i = 255;
   if (i < 0) i = 0;
 
   return static_cast<unsigned char>(i);
 }
 
-bool SaveImage(const char* filename, const float* rgba, float scale, int width, int height) {
+bool SaveImage(const char* filename, const float* rgba, float scale, float gamma, int width, int height) {
 
   std::vector<unsigned char> dst(width * height * 4);
 
   // alpha channel is also affected by `scale` parameter.
   for (size_t i = 0; i < width * height * 4; i++) {
-    dst[i] = ftouc(rgba[i] * scale);
+    dst[i] = ftouc(rgba[i] * scale, gamma);
   }
 
   int ret = stbi_write_png(filename, width, height, 4, static_cast<const void*>(dst.data()), width * 4);
@@ -36,10 +36,11 @@ bool SaveImage(const char* filename, const float* rgba, float scale, int width, 
 int main(int argc, char** argv)
 {
   if (argc < 3) {
-    printf("Usage: exr2ldr input.exr output.png (scale) (resize_factor).\n");
+    printf("Usage: exr2ldr input.exr output.png (scale) (resize_factor) (gammavalue).\n");
     printf("    Pixel value [0.0, 1.0] in EXR is mapped to [0, 255] for LDR image.\n");
     printf("    You can adjust pixel value by `scale`(default = 1.0).\n");
     printf("    Resize image using `resize_factor`(default = 1.0). 2 = create half size image, 4 = 1/4 image, and so on\n");
+    printf("    gammmavalue will be used for gamma correction when saving png image(default = 1.0).\n");
     exit(-1);
   }
 
@@ -51,6 +52,11 @@ int main(int argc, char** argv)
   float resize_factor = 1.0f;
   if (argc > 4) {
     resize_factor = atof(argv[4]);
+  }
+
+  float gamma = 1.0f;
+  if (argc > 5) {
+    gamma = atof(argv[5]);
   }
     
   int width, height;
@@ -73,7 +79,7 @@ int main(int argc, char** argv)
   int ret = stbir_resize_float(rgba, width, height, width*4*sizeof(float), &buf.at(0), dst_width, dst_height,dst_width*4*sizeof(float), 4);
   assert(ret != 0);
 
-  bool ok = SaveImage(argv[2], &buf.at(0), scale, dst_width, dst_height);
+  bool ok = SaveImage(argv[2], &buf.at(0), scale, gamma, dst_width, dst_height);
 
   return (ok ? 0 : -1);
 }
