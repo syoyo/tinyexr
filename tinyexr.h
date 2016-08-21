30a70c0febba0b5a49a390eaa2675b9a9f8d7d76
Syoyo Fujita <syoyo@lighttransport.com>
2016-08-21 19:22:55 +0900

    Disable TIME support for MINIZ since sme environment does not support utime() and we don't neet it anyway.

--- tinyexr.h
+++ tinyexr.h
@@ -745,7 +745,7 @@ namespace miniz {
 // get/set file times, and the C run-time funcs that get/set times won't be
 // called.
 // The current downside is the times written to your archives will be from 1979.
-//#define MINIZ_NO_TIME
+#define MINIZ_NO_TIME
 
 // Define MINIZ_NO_ARCHIVE_APIS to disable all ZIP archive API's.
 //#define MINIZ_NO_ARCHIVE_APIS
