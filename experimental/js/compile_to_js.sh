4e254245417b70ad93f95d2ccc24a0a16cf2aa52
Syoyo Fujita <syoyo@lighttransport.com>
2022-11-05 20:55:57 +0900

    Update Emscripten build of TinyEXR loader.

--- experimental/js/compile_to_js.sh
+++ experimental/js/compile_to_js.sh
@@ -6,4 +6,7 @@ EMCC=em++
 # TODO: export more functions
 # DEMANGLE_SUPPORT=1
 #${EMCC} -std=c++11 --bind -O2 -I../../ binding.cc --memory-init-file 0 -s TOTAL_MEMORY=67108864 -s DEMANGLE_SUPPORT=1 -s EXPORTED_FUNCTIONS="['_ParseEXRHeaderFromMemory', '_LoadEXRFromMemory']" -o tinyexr.js
-${EMCC} --bind -Os -I../../ binding.cc --memory-init-file 0 -s TOTAL_MEMORY=67108864 -o tinyexr.js
+#${EMCC} --bind -Os -I../../ -I../../deps/miniz binding.cc ../../deps/miniz/miniz.c --memory-init-file 0 -s TOTAL_MEMORY=67108864 -o tinyexr.js
+
+${EMCC} --bind -Os -I../../ -I../../deps/miniz binding.cc ../../deps/miniz/miniz.c -s ALLOW_MEMORY_GROWTH=1 -o tinyexr.js
+
