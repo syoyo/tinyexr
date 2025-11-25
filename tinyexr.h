4c08a85910228b0a8c6cfdc8e7b4533462b349a2
Stefan Werner <stefan.werner@intel.com>
2025-11-25 12:49:20 +0100

    Fixed leaked header memory when image loading fails.

--- tinyexr.h
+++ tinyexr.h
@@ -6633,6 +6633,7 @@ int LoadEXRFromMemory(float **out_rgba, int *width, int *height,
   InitEXRImage(&exr_image);
   ret = LoadEXRImageFromMemory(&exr_image, &exr_header, memory, size, err);
   if (ret != TINYEXR_SUCCESS) {
+    FreeEXRHeader(&exr_header);
     return ret;
   }
 
